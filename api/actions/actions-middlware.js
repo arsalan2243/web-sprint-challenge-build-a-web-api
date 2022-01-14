fedd9f04b81276d10cf9b8638368bfa0aa7ac781
arsalan rahimi <years1373@yahoo.com>
2022-01-14 15:40:37 -0800

    action get middle ware completed

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,17 @@
 // add middlewares here related to actions
+const Action = require("./actions-model")
+const checkId = async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const found = await Action.get(id)
+    if (!found) {
+      next({ message: "Id not found" })
+    } else {
+      req.found = found
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+module.exports = { checkId }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,26 @@
 // Write your "actions" router here!
+const express = require("express")
+const router = express.Router()
+const Action = require("./actions-model")
+const { checkId } = require("./actions-middlware.js")
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allActions = await Action.get()
+    res.status(200).json(allActions)
+  } catch (error) {
+    next(error)
+  }
+})
+router.get("/:id", checkId, async (req, res) => {
+  res.status(200).json(req.found)
+})
+router.post("/", (req, res, next) => {})
+// router.get("/", (req, res, next) => {
+
+// })
+// router.get("/", (req, res, next) => {
+
+// })
+
+module.exports = router
