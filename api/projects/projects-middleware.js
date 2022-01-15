6c545da7166b71ffbf0dec3fcadcde0280cd517a
arsalan rahimi <years1373@yahoo.com>
2022-01-14 16:56:19 -0800

    finished work

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,60 @@
-// add middlewares here related to projects
+const { get } = require("./projects-model")
+const yup = require("yup")
+const checkId = async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const found = await get(id)
+    if (!found) {
+      next({ message: `sorry that the id of ${id} doesn't exist` })
+    } else {
+      req.found = found
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+
+const vaidatetion = yup.object().shape({
+  name: yup.string().required().trim(),
+  description: yup.string().required().trim(),
+})
+
+const validateProject = async (req, res, next) => {
+  try {
+    const validatebody = await vaidatetion.isValid(req.body)
+    if (!validatebody) {
+      res.status(400).json({ message: "name and description are required" })
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+
+const changeValid = yup.object().shape({
+  name: yup.string().required().trim(),
+  description: yup.string().required().trim(),
+  completed: yup.boolean().required(),
+})
+const validateChange = async (req, res, next) => {
+  try {
+    const validatebody = await changeValid.isValid(req.body)
+    if (!validatebody) {
+      res
+        .status(400)
+        .json({ message: "name and description and completed are required" })
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = {
+  checkId,
+  validateProject,
+  validateChange,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,4 +1,49 @@
-// Write your "projects" router here!
+const Project = require("./projects-model")
 const express = require("express")
+
 const router = express.Router()
+const {
+  checkId,
+  validateProject,
+  validateChange,
+} = require("./projects-middleware")
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allProjects = await Project.get()
+    res.status(200).json(allProjects)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get("/:id", checkId, (req, res) => {
+  res.status(200).json(req.found)
+})
+
+router.post("/", validateProject, async (req, res) => {
+  const newProject = await Project.insert(req.body)
+  res.status(201).json(newProject)
+})
+
+router.put("/:id", checkId, validateChange, async (req, res) => {
+  const updated = await Project.update(req.params.id, req.body)
+  res.status(200).json(updated)
+})
+
+router.delete("/:id", checkId, async (req, res) => {
+  await Project.remove(req.params.id)
+  res.status(200).json([
+    {
+      message: "project is deleted",
+      deleted_project: req.found,
+    },
+  ])
+})
+
+router.get("/:id/actions", checkId, async (req, res) => {
+  const projectActions = await Project.getProjectActions(req.params.id)
+  res.status(200).json(projectActions)
+})
+
 module.exports = router
